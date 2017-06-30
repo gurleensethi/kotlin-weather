7b5ad12fea8434707c70ef2ebeabf42f7e118bdd
gurleensethi <sarusethi@rocketmail.com>
2017-06-30 13:34:45 +0530

    Prepare for release

--- app/src/main/java/app/com/thetechnocafe/kotlinweather/Consts/Secrets.kt
+++
@@ -1,8 +0,0 @@
-package app.com.thetechnocafe.kotlinweather.Consts
-
-/**
- * Created by gurleensethi on 30/06/17.
- */
-object Secrets {
-    val API_KEY = "fIuqZsQ5PHmshV6MXhrpVzdbLTtzp1oR9LLjsnU1Yo900JH8pM"
-}
